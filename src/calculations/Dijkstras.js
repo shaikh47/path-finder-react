983db1e6b4be94c18b64013fefc871f5574b5bfb
omar shaikh <omar.shaikh@bjitgroup.com>
2023-06-02 17:47:27 +0600

    added useeffect to traverse through the segments, added function to get the adjacent segments of a selected segment

---
+++ src/calculations/Dijkstras.js
@@ -0,0 +1,100 @@
+export const calculate = (
+  segmentNumber,
+  beginning,
+  destination,
+  barriers,
+  maxColumns,
+  maxSegments
+) => {
+  return getAdjacent(segmentNumber, maxColumns, maxSegments);
+};
+
+function toFindDuplicates(arry) {
+  const uniqueElements = new Set(arry);
+  const filteredElements = arry.filter(item => {
+      if (uniqueElements.has(item)) {
+          uniqueElements.delete(item);
+      } else {
+          return item;
+      }
+  });
+
+  return [...new Set(uniqueElements)]
+}
+
+export const dijkstras = (
+  beginning,
+  destination,
+  barriers,
+  maxColumns,
+  maxSegments
+) => {
+  const visitedVertices = [];
+  const unvisitedVertices = [];
+
+  const scanned = [beginning];
+
+  for(let i=0;i<=336;i++) {
+    getAdjacent(scanned[i], maxColumns, maxSegments).forEach((item, index)=> {
+      if(!scanned.includes(item)) {
+        scanned.push(item);
+      }
+    });
+  }
+
+  console.log("duplicates: ", toFindDuplicates(scanned))
+  return scanned;
+};
+
+const getAdjacent = (segmentNumber, maxColumns, maxSegments) => {
+  const result = {};
+  const resultArr = [];
+
+  const upperSegment = segmentNumber - maxColumns;
+  const lowerSegment = segmentNumber + maxColumns;
+
+  const leftSegment = segmentNumber - 1;
+  const rightSegment = segmentNumber + 1;
+
+  const topLeftCornerSegment = upperSegment - 1;
+  const topRightCornerSegment = upperSegment + 1;
+  const bottomLeftCornerSegment = lowerSegment - 1;
+  const bottomRightCornerSegment = lowerSegment + 1;
+
+  if (upperSegment >= 0) {
+    result.upperSegment = upperSegment;
+    resultArr.push(upperSegment);
+  }
+  if (lowerSegment <= maxSegments) {
+    result.lowerSegment = lowerSegment;
+    resultArr.push(lowerSegment);
+  }
+  if (segmentNumber % maxColumns !== 0) {
+    result.leftSegment = leftSegment;
+    resultArr.push(leftSegment);
+
+    if (result.upperSegment !== undefined) {
+      result.topLeftCornerSegment = topLeftCornerSegment;
+      resultArr.push(topLeftCornerSegment);
+    }
+    if (result.lowerSegment !== undefined) {
+      result.bottomLeftCornerSegment = bottomLeftCornerSegment;
+      resultArr.push(bottomLeftCornerSegment);
+    }
+  }
+  if (rightSegment % maxColumns !== 0) {
+    result.rightSegment = rightSegment;
+    resultArr.push(rightSegment);
+
+    if (result.upperSegment !== undefined) {
+      result.topRightCornerSegment = topRightCornerSegment;
+      resultArr.push(topRightCornerSegment);
+    }
+    if (result.lowerSegment !== undefined) {
+      result.bottomRightCornerSegment = bottomRightCornerSegment;
+      resultArr.push(bottomRightCornerSegment);
+    }
+  }
+
+  return resultArr;
+};
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,25 +1,141 @@
-import { useState } from "react";
-import styles from './Canvas.module.css';
+import { useState, useRef, useEffect } from "react";
+import styles from "./Canvas.module.css";
+import { calculate, dijkstras } from "../calculations/Dijkstras";
+
+const divColors = [
+  "#5f6ce1",
+  "#2bdd13",
+  "#0ee2d1",
+  // Add more colors here
+];
 
 function Canvas() {
   const [barrier, setBarrier] = useState([]);
+  const isClicked = useRef(false);
+
+  const [beginning, setBeginning] = useState(140);
+  const [destination, setDestination] = useState(167);
+
+  const [currentSegment, setCurrentSegment] = useState(0);
+  const [color, setColor] = useState(2);
+
+  const [colorArr, setColorArr] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSegment((prevIndex) => (prevIndex + 1) % 336);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (colorArr.length !== 0) return;
+    const tempArr = [];
+    for (let i = 0; i < 336; i++) {
+      tempArr.push("white");
+    }
+    setColorArr(tempArr);
+    console.log(colorArr);
+  }, []);
+
+  useEffect(() => {
+    console.log(colorArr);
+    console.log(dijkstras(140, -1, -1, 28, 336));
+    
+  }, [colorArr]);
+
+  const handlSegmentDrag = (key) => {
+    if (key === beginning || key === destination) return;
+    if (!isClicked.current) return;
+    setBarrier((prevBarriers) => {
+      if (!prevBarriers.includes(key)) {
+        return [...prevBarriers, key];
+      }
+      return prevBarriers;
+    });
+  };
 
   const handlSegmenteClick = (key) => {
-    console.log('Clicked item key:', barrier);
-    setBarrier((prevBarriers) => [...prevBarriers, key]);
+    console.log(calculate(key, beginning, destination, barrier, 28, 335));
+
+    // change the side segment colors
+
+    if (key === beginning || key === destination) return;
+
+    console.log("Clicked item key:", barrier);
+    setBarrier((prevBarriers) => {
+      if (!prevBarriers.includes(key)) {
+        return [...prevBarriers, key];
+      }
+      return prevBarriers;
+    });
+  };
+
+  const updateColor = (index, newColor) => {
+    setColorArr((prevColorArr) => {
+      const updatedArr = [...prevColorArr];
+      updatedArr[index] = newColor;
+      return updatedArr;
+    });
+  };
+
+  const handleBeginningClick = (key) => {};
+
+  const handleDestinationClick = (key) => {};
+
+  const handleMouseUp = () => {
+    isClicked.current = false;
+  };
+
+  const handleMouseDown = () => {
+    isClicked.current = true;
   };
 
   const renderSegments = () => {
     const elements = [];
     for (let i = 0; i < 336; i++) {
-      elements.push(<div style={{
-        backgroundColor: barrier.includes(i) ? "red" : ""
-      }} className={`${styles.segment}`} key={i} onClick={() => handlSegmenteClick(i)}><p>{i}</p></div>);
+      elements.push(
+        <div
+          style={{
+            // backgroundColor: (beginning===i ? (destination===i?"blue":"") : ""),
+            backgroundColor: barrier.includes(i)
+              ? "red"
+              : beginning === i
+              ? "green"
+              : destination === i
+              ? "yellow"
+              : currentSegment === i
+              ? divColors[color]
+              : colorArr[i]
+
+            // backgroundColor: colorArr[i],
+          }}
+          className={`${styles.segment}`}
+          key={i}
+          onMouseEnter={() => handlSegmentDrag(i)}
+          onMouseUp={() => handleMouseUp(i)}
+          onMouseDown={() => handleMouseDown(i)}
+          onClick={() => handlSegmenteClick(i)}
+        >
+          <p>{i}</p>
+        </div>
+      );
     }
     return elements;
   };
 
-  return <div className={`${styles.container}`}>{renderSegments()}</div>;
+  return (
+    <div className={`${styles.container}`}>
+      <div className={`${styles.canvasContainer}`}>{renderSegments()}</div>
+      <div className={`${styles.buttonContainer}`}>
+        <button className={`${styles.buttons}`}>Add Beginning</button>
+        <button className={`${styles.buttons}`}>Add Destination</button>
+      </div>
+    </div>
+  );
 }
 
 export default Canvas;
